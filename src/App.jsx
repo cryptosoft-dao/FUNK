3272618d16d57f0ae902466d5ec3cfb3eeacd7dd
vadimilyano <vadimilyano235@gmail.com>
2024-07-13 13:10:05 +0300

    F-clicker main

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Layout from "./layout/Layout";
+import Earn from "./pages/Earn";
+
+function App() {
+  return (
+    <div className="main-screen">
+      <div className="upper-part" style={{ backgroundColor: 'white', height: '50vh' }} />
+      <div className="lower-part" style={{ backgroundColor: 'black', height: '50vh' }} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Earn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Earn(props) {
+    return (
+        <div className='container earn_'>
+            <div className="up">1</div>
+            <div className="down">2</div>
+        </div>
+    );
+}
+
+export default Earn;
